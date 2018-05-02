096941aae17224d828e8748abb4fc1bb0f91f5a6
leonleo997 <leonleo997@gmail.com>
2018-05-02 10:02:44 -0400

    Upload Flask Files

---
+++ A00056408/op_stats/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/vi/stats/cpu')
+
+def get_cpu_percent():
+  cpu_percent = '100'
+  #return {'cpu_percent': cpu_percent}
+  return '{}'.format(cpu_percent)
+
+if __name__=='__main__':
+ app.run(host='0.0.0.0', port=8080)
